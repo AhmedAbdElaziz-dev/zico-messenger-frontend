84e2d6c3b39c551475f1d0e673ca4b8308bf032d
ahmedAbdElaziz <a.abdelaziz.hamed@gmail.com>
2020-06-17 17:37:29 +0200

    Fix icon component style

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -4,8 +4,8 @@ import styled from "styled-components";
 const Icon = ({ src }) => <IconImg src={src} />;
 
 const IconImg = styled.img`
-  width: 20px;
-  height: 20px;
+  width: 3rem;
+  height: 3rem;
   margin-right: 2rem;
 `;
 
