b6cdf10e0fa8a589752753cbd3bbe16ccb079b4c
ahmedAbdElaziz <a.abdelaziz.hamed@gmail.com>
2020-06-17 17:32:16 +0200

    Fix form component style

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -3,29 +3,56 @@ import styled from "styled-components";
 import Input from "../Input";
 import Button from "../../components/Button";
 
-const Form = () => (
-  <FormContainer>
-    <FormInputs>
-      <Input type={"text"} palceholder={"Enter your name"} label={"username"} />
-      <Input
-        type={"text"}
-        palceholder={"Enrer you password"}
-        label={"password"}
-      />
+const Form = ({
+  formHeader,
+  inputList,
+  buttonList,
+  formWidth,
+  inputWidth,
+  type,
+}) => (
+  <FormContainer formWidth={formWidth}>
+    <FormHead>{formHeader}</FormHead>
+    <FormInputs type={type}>
+      {inputList.map((inputItem) => (
+        <Input
+          inputWidth={inputWidth}
+          type={inputItem.type}
+          label={inputItem.label}
+        />
+      ))}
     </FormInputs>
     <FormButtons>
-      <Button  />
+      {buttonList.map((buttonItem) => (
+        <Button text={buttonItem.text} />
+      ))}
     </FormButtons>
   </FormContainer>
 );
-const FormInputs = styled.div``;
-const FormButtons = styled.div``;
+const FormInputs = styled.div`
+  ${(props) =>
+    props.type === "register" &&
+    `
+  width:100%;
+  display:flex;
+  flex-wrap:wrap;
 
+  `}
+`;
+const FormButtons = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+`;
+const FormHead = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 800;
+`;
 const FormContainer = styled.form`
   margin: 3rem 0rem;
-  padding: 3rem 1rem;
+  padding: 1rem;
   display: flex;
-  width: 10rem;;
+  width: ${(props) => props.formWidth};
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
@@ -33,7 +60,7 @@ const FormContainer = styled.form`
   border: 2px solid #efefef;
   border-radius: 10px;
   @media (min-width: 1024px) {
-    width: 25%;
+    width: 80%;
   }
 `;
 
