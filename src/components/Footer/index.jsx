9d442c7b7097e637a3b93baf77288fc8c1fd407c
ahmedAbdElaziz <a.abdelaziz.hamed@gmail.com>
2020-06-17 11:07:19 +0200

    Add footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = () => <FooterContainer />;
+
+const FooterContainer = styled.div`
+  width: 100%;
+  height: 7rem;
+  box-sizing: border-box;
+  background-image: url("image7.webp");
+  background-size: cover;
+  background-position: center;
+  background-color: #0c121c;
+  background-repeat: no-repeat;
+`;
+
+export default Footer;
