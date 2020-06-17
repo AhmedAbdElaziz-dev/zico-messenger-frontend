3aa4b27a22a5650e86aca98399d99a6283ee72e6
ahmedAbdElaziz <a.abdelaziz.hamed@gmail.com>
2020-06-17 10:59:19 +0200

    Add Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import Navbar from "../Navbar";
+
+const Header = () => (
+  <HeaderContainer>
+    <Navbar></Navbar>;
+  </HeaderContainer>
+);
+
+const HeaderContainer = styled.div`
+  width:100%;
+  background-color:#EFEFEF;  
+  display: flex;
+  justify-content:center;
+  align-items-center;
+  padding:0.5rem;
+`;
+
+export default Header;
